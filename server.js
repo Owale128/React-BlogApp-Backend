42dbc39de413264b7a2481353a37de35b9d76ae9
Owale128 <Christian.buaka@hotmail.com>
2024-03-21 12:43:46 +0100

    Added a delete route and also tested it in api.rest file

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient} = require("mongodb");
+const { MongoClient, ObjectId} = require("mongodb");
 const app = express();
 const port = 3000;
 require('dotenv').config();
@@ -40,7 +40,25 @@ res.status(500).json({ error: "Internal Server Error"});
 
 });
 
-
+app.delete('/api/blogPost/:id', async(req, res) =>{
+  
+  try {
+    await client.connect();
+    const collection = client.db('Blog').collection('BlogPosts');
+    const postId = req.params.id;
+    const result = await collection.deleteOne({_id: new ObjectId(postId) });
+    if (result.deletedCount ===0) {
+      res.status(404).json({ error: "Blog post not found"});
+    } else {
+      res.status(204).json({ message: "Blog post successfully deleted!" });
+    }
+  } catch (error) {
+    console.error("Error deleting blog post", error);
+    res.status(500).json({ error: "internal Server Error"});
+  } finally {
+    await client.close()
+  }
+})
 
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
