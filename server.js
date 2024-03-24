4b5480ebde0684d06568604dc0a95116acbf89c8
Owale128 <Christian.buaka@hotmail.com>
2024-03-24 17:31:11 +0100

    Added a post route

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 app.use(express.json());
 app.use(cors());
 
+
 app.get('/api/blogPosts', async (req, res) => {
     try {
       await client.connect();
@@ -25,8 +26,8 @@ app.get('/api/blogPosts', async (req, res) => {
     }
   });
   
-app.post('/api/blogPost', async (req,res)=>{
 
+app.post('/api/blogPost', async (req,res)=>{
 try {
   await client.connect();
   const collection = client.db('Blog').collection('BlogPosts');
@@ -40,8 +41,32 @@ res.status(500).json({ error: "Internal Server Error"});
 } finally {
   await client.close();
 }
-
 });
+
+
+app.put('/api/blogPost/:id', async (req, res) => {
+  try {
+    await client.connect();
+    const collection = client.db('Blog').collection('BlogPosts');
+    const postId = req.params.id;
+    const updatedPost = req.body;
+    const result = await collection.updateOne(
+      { _id: new ObjectId(postId) },
+      { $set: updatedPost }
+    );
+    if (result.modifiedCount === 0) {
+      res.status(404).json({ error: "Blog post not found" });
+    } else {
+      res.status(200).json({ message: "Blog post successfully updated!" });
+    }
+  } catch (error) {
+    console.error("Error updating blog post:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  } finally {
+    await client.close();
+  }
+});
+
 
 app.delete('/api/blogPost/:id', async(req, res) =>{
   
